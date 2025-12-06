730d5e61a1e26bebdfbf76f544dc8dc8faf3ab4c
Sai Kumar Siruvati <155241059+Saikumarsiruvati@users.noreply.github.com>
2025-12-06 11:37:29 +0530

    Delete revtickets-backend directory

--- revtickets-backend/src/main/java/com/revtickets/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.revtickets.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- revtickets-backend/src/main/java/com/revtickets/exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.revtickets.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
